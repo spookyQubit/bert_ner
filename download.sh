0e052e50d2bba492c164ded9d5b3af742a882439
Shantanu Agarwal <iitkgp.shantanu@gmail.com>
2019-10-19 22:48:28 -0400

    Update download.sh
    Hi Kyubyong, Thanks for the amazing work. The directory structure of NeuroNER's repository seems to have changed. This commit is to point train/valid/test.txt files to the correct location.

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-train=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/master/data/conll2003/en/train.txt
-valid=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/master/data/conll2003/en/valid.txt
-test=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/master/data/conll2003/en/test.txt
+train=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/master/neuroner/data/conll2003/en/train.txt
+valid=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/master/neuroner/data/conll2003/en/valid.txt
+test=https://raw.githubusercontent.com/Franck-Dernoncourt/NeuroNER/master/neuroner/data/conll2003/en/test.txt
 
 mkdir conll2003 | wget --show-progress $train && mv train.txt conll2003
 wget --show-progress $valid && mv valid.txt conll2003
